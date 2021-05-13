f77dddcfbab23b5b61b0bd71cd9d5ee565f06c61
Taylor Ludwig <taylorludwig@gmail.com>
2021-05-13 11:06:56 -0700

    read version file directly

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as req_file:
     REQUIREMENTS = req_file.read()
 
+with open(".version") as version_file:
+    VERSION = version_file.read()
+
 setup(
     name="canu",
     author="Brooks Vinyard",
     author_email="brooks.vinyard@broadwing.io",
     description="CSM Automatic Network Utility",
     long_description="CANU floats through a new Shasta network and makes setup a breeze.",
-    version="0.0.4",
+    version=VERSION,
     py_modules=["canu"],
     packages=find_packages(exclude=("tests",)),
     package_data={"canu": ["canu.yaml", "canu/canu.yaml"]},
