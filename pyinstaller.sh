2fe583cbb55723101999e7270f2f78cfafd06835
Taylor Ludwig <taylorludwig@gmail.com>
2021-05-28 13:43:06 -0700

    new rpm build following updated github logic

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Pyinstaller needs a .spec file but it conflicts with rpm builds
+cp pyinstaller.py pyinstaller.spec
 pyinstaller --clean -y --dist ./dist/linux --workpath /tmp pyinstaller.spec
+rm pyinstaller.spec
 chown -R --reference=. ./dist/linux
