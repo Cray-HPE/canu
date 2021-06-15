65d224e9a70638904e7e92e8f4dca13a729d8a8a
batesl <lucas.bates@hpe.com>
2021-06-15 11:03:53 -0500

    added more port configs

--- network_modeling/configs/ncn_configs.py
+++ network_modeling/configs/ncn_configs.py
@@ -9,7 +9,7 @@ import yamale
 
 cabling_file = "../cabling/standards/cabling.yaml"
 yamale_schema = "../cabling/standards/cabling_schema.yaml"
-template_file = "ncn_port_config.j2"
+template_file = "edge_port_config.j2"
 
 schema = yamale.make_schema(yamale_schema)
 data = yamale.make_data(cabling_file)
@@ -32,19 +32,48 @@ for node in cabling["nodes"]:
         node["config"] = {
             "description": "ncn-m001 port mgmt0",
             "lag_number": "1",
-            "interface": "1/1/1",
+            "interface_lag": "1/1/1",
         }
     if node["subtype"] == "worker":
         node["config"] = {
             "description": "ncn-w001 port mgmt0",
             "lag_number": "2",
-            "interface": "1/1/2",
+            "interface_lag": "1/1/2",
         }
     if node["subtype"] == "storage":
         node["config"] = {
             "description": "ncn-s001 port mgmt0",
             "lag_number": "3",
-            "interface": "1/1/3",
+            "interface_lag": "1/1/3",
+        }
+    if node["subtype"] == "uan":
+        node["config"] = {
+            "description": "uan",
+            "lag_number": "4",
+            "interface_lag": "1/1/4",
+            "interface_nmn": "1/1/5",
+        }
+    if node["subtype"] == "login":
+        node["config"] = {
+            "description": "login node",
+            "lag_number": "5",
+            "interface_lag": "1/1/6",
+        }
+    if node["subtype"] == "cec":
+        node["config"] = {
+            "description": "cec",
+            "interface_lag": "1/1/7",
+        }
+    if node["subtype"] == "cmm":
+        node["config"] = {
+            "description": "cmm",
+            "interface_lag": "1/1/8",
+            "lag_number": "6",
+        }
+    if node["subtype"] == "bmc":
+        node["config"] = {
+            "description": "bmc",
+            "interface": "1/1/9",
         }
 
 pprint.pprint(cabling["nodes"])
