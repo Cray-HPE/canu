72f91c3a8783a5adeaa377be55887349d0764a88
lukebates123 <lucas.bates@hpe.com>
2022-01-04 15:16:21 -0600

    add metadata+doc string

--- hook-nornir.py
+++ hook-nornir.py
@@ -1,4 +1,5 @@
-from PyInstaller.utils.hooks import collect_submodules
-
+"""nornir hook file."""
+from PyInstaller.utils.hooks import collect_submodules, copy_metadata
 
 hiddenimports = collect_submodules("nornir")
+datas = copy_metadata("nornir")
