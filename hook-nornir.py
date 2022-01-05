7e8577dde58ca9aa382f0f250ed48e44344c6321
lukebates123 <lucas.bates@hpe.com>
2022-01-05 13:18:34 -0600

    fix pyinstaller hooks

--- hook-nornir.py
+++ hook-nornir.py
@@ -1,4 +1,24 @@
 """nornir hook file."""
-from PyInstaller.utils.hooks import collect_all
+from PyInstaller.utils.hooks import (
+    collect_dynamic_libs,
+    collect_submodules,
+    copy_metadata,
+)
 
-datas, binaries, hiddenimports = collect_all("nornir", "nornir_salt", "nornir-netmiko")
+datas = (
+    copy_metadata("nornir")
+    + copy_metadata("nornir_salt")
+    + copy_metadata("nornir_netmiko")
+)
+
+hiddenimports = (
+    collect_submodules("nornir")
+    + collect_submodules("nornir_salt")
+    + collect_submodules("nornir_netmiko")
+)
+
+binaries = (
+    collect_dynamic_libs("nornir")
+    + collect_dynamic_libs("nornir_salt")
+    + collect_dynamic_libs("nornir_netmiko")
+)
