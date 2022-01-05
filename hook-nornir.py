3926509fad99355c99de45e2fc94842bc6839504
lukebates123 <lucas.bates@hpe.com>
2022-01-05 11:32:15 -0600

    add nornir-salt to hooks

--- hook-nornir.py
+++ hook-nornir.py
@@ -1,5 +1,6 @@
 """nornir hook file."""
 from PyInstaller.utils.hooks import collect_submodules, copy_metadata
 
-hiddenimports = collect_submodules("nornir")
-datas = copy_metadata("nornir")
+datas = copy_metadata("nornir") + copy_metadata("nornir_salt")
+
+hiddenimports = collect_submodules("nornir") + collect_submodules("nornir_salt")
