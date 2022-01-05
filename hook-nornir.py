ef681f5f152a2abd42945155eb39cf4979db5aa8
lukebates123 <lucas.bates@hpe.com>
2022-01-05 12:32:23 -0600

    pyinstaller hook

--- hook-nornir.py
+++ hook-nornir.py
@@ -1,4 +1,12 @@
 """nornir hook file."""
-from PyInstaller.utils.hooks import collect_all
+from PyInstaller.utils.hooks import (
+    collect_dynamic_libs,
+    collect_submodules,
+    copy_metadata,
+)
 
-datas, binaries, hiddenimports = collect_all("nornir") + collect_all("nornir_salt")
+datas = copy_metadata("nornir") + copy_metadata("nornir_salt")
+
+hiddenimports = collect_submodules("nornir") + collect_submodules("nornir_salt")
+
+binaries = collect_dynamic_libs("nornir") + collect_dynamic_libs("nornir_salt")
