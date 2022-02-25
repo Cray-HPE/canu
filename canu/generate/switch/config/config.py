08ed8b1f858aeabbe1bbe852955026cd1006f65f
lukebates123 <lucas.bates@hpe.com>
2022-02-25 16:13:24 -0600

    fix gen switch config

--- canu/generate/switch/config/config.py
+++ canu/generate/switch/config/config.py
@@ -1336,9 +1336,6 @@ def parse_sls_for_config(input_json):
                 "MyASN",
                 {},
             )
-            sls_variables["NMN_NETMASK"] = sls_variables["NMN"].netmask
-            sls_variables["NMN_PREFIX_LEN"] = sls_variables["NMN"].prefixlen
-            sls_variables["NMN_NETWORK_IP"] = sls_variables["NMN"].ip
             for subnets in sls_network.get("ExtraProperties", {}).get("Subnets", {}):
                 if subnets["Name"] == "bootstrap_dhcp":
                     for ip in subnets["IPReservations"]:
