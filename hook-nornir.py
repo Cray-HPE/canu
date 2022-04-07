c75e607c3a8e0479738ef98e763b251c6568ae01
lukebates123 <lucas.bates@hpe.com>
2022-04-07 18:50:41 -0500

    rpm test

--- hook-nornir.py
+++ hook-nornir.py
@@ -19,7 +19,7 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-"""pyinstaller hook file."""
+"""nornir hook file."""
 from PyInstaller.utils.hooks import (
     collect_dynamic_libs,
     collect_submodules,
