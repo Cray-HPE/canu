62934937aa3301885a30c58c60b8c1d583375af1
Sean Lynn <sean.lynn@hpe.com>
2022-05-17 13:27:58 -0700

    CASMNET-1541 - Add 4 port 1G OCP card nodes and bugfixes (#149)
    * merge develop
    
    * Read only values, not formulas in Excel.
    
    * Add dvs and ssn node types, and add gn to gateway.
    
    * Change lmem naming lm to lmem.
    
    * Add 4 port 1G templates for dvs, gateway etc... nodes.  Change port descriptions and related tests.
    
    * Bump version.
    
    * Update readme.md
    
    * add lnet 1gb ocp
    
    Co-authored-by: lukebates123 <lucas.bates@hpe.com>
    Co-authored-by: lukebates123 <66981180+lukebates123@users.noreply.github.com>

--- canu/generate/network/config/config.py
+++ canu/generate/network/config/config.py
@@ -148,6 +148,12 @@ csm_options = canu_config["csm_versions"]
     help="Path to current running configs.",
     type=click.Path(),
 )
+@click.option(
+    "--reorder",
+    is_flag=True,
+    help="reorder config to heir config order",
+    required=False,
+)
 @click.pass_context
 def config(
     ctx,
@@ -163,6 +169,7 @@ def config(
     folder,
     preserve,
     custom_config,
+    reorder,
 ):
     """Generate the config of all switches (Aruba, Dell, or Mellanox) on the network using the SHCD.
 
@@ -220,6 +227,7 @@ def config(
         folder: Folder to store config files
         preserve: Folder where switch running configs exist.  This folder should be populated from the "canu backup network" command.
         custom_config: yaml file containing customized switch configurations which is merged with the generated config.
+        reorder: Filters generated configurations through hier_config generate a more natural running-configuration order.
     """
     # SHCD Parsing
     if shcd:
@@ -389,6 +397,7 @@ def config(
                 vendor_folder,
                 preserve,
                 custom_config,
+                reorder,
             )
             all_unknown.extend(unknown)
             config_devices.update(devices)
