1bcdb490d6174d43fa98c15620bcb38a2f469743
lukebates123 <lucas.bates@hpe.com>
2022-05-27 15:26:30 -0500

    Revert "JOUNI LOVES TESTS MORE THAN ANYTHING"
    This reverts commit 318e89e8b171769efd9c43d4f9f35bb898a92cb4.

--- tests/app.py
+++
@@ -1,8 +0,0 @@
-# contents of app.py, a simple API retrieval example
-import requests
-
-
-def get_json(url):
-    """Takes a URL, and returns the JSON."""
-    r = requests.get(url)
-    return r.json()
