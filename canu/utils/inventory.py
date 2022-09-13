d71233b88e06eee04e7c8c5f9e34760f5b032b44
lukebates123 <66981180+lukebates123@users.noreply.github.com>
2022-09-13 13:26:47 -0500

    Casmnet 1808 (#198)
    * sls library and ping test
    
    * add dellanox tests
    
    * add to test framework
    
    * lint
    
    * readme/version bump
    
    * docs
    
    * edit noxfile
    
    * fix bgp docs
    
    * fix docs
    
    Co-authored-by: lukebates123 <lucas.bates@hpe.com>
    Co-authored-by: Sean Lynn <sean.lynn@hpe.com>

--- canu/utils/inventory.py
+++ canu/utils/inventory.py
@@ -20,10 +20,6 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 """Create Nornir Inventory from SLS."""
-import json
-
-import click
-
 from canu.utils.sls import pull_sls_hardware, pull_sls_networks
 
 
@@ -31,16 +27,8 @@ def inventory(username, password, network, sls_json=None, sls_inventory=None):
     """Build Nornir inventory from sls_input."""
     inventory = {"groups": {}, "hosts": {}}
     if sls_json:
-        try:
-            input_json = json.load(sls_json)
-        except (json.JSONDecodeError, UnicodeDecodeError):
-            click.secho(
-                f"The file {sls_json.name} is not valid JSON.",
-                fg="red",
-            )
-            return
-        sls_variables = pull_sls_networks(input_json)
-        sls_hardware = pull_sls_hardware(input_json)
+        sls_variables = pull_sls_networks(sls_json)
+        sls_hardware = pull_sls_hardware(sls_json)
     else:
         sls_variables = pull_sls_networks()
         sls_hardware = pull_sls_hardware()
